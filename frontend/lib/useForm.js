5aa6f09dc7405258bc12e4dfcf85f8671ad762b9
Zac Edens <themancalledzac@gmail.com>
2021-05-01 11:58:02 -0700

    useForm hook created, also formatMoney

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    // if it's a number, we want changes to that number to remain a number and not a string
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    // if it's a file, it requires an array of files
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      // basically using a variable for what we are targeting
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  // if you want to surface either functionality or data from a custom hook, you must return it from that custom hook
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
